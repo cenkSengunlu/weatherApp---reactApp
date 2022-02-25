ae9b9dd60d9311b6b2a2468f685f8b12ccd93e0d
Cenk Şengünlü <cenksengunlu@hotmail.com>
2022-02-25 19:48:32 +0300

    add theme change and add error messages

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import cssThemeObject from '../cssThemeObject';
 
 const WeatherComponent = () => {
     const apiKey = "bfc5a840b7772812d48cf0b2a6437494";
     const limit = 1;
+    // Input içerisindeki değeri tut
     const [inputVal, setInputVal] = useState("");
+
+    // Input'tan gelen değer atanacak
     const [cityName, setCityName] = useState("");
     
+    // Geocoding Api'ından gelecek objeyi tut
     const [cityItem, setCityItem] = useState(null);
 
+    // Openweather Api'ından gelecek objeyi tut
     const [weatherItem, setWeatherItem] = useState(null);
 
+     // Tema belirleyici toggle'ın durumunu tut
+    const [checked, setChecked] = useState(false);
+
+    // Input'taki veriyi al
     const getValue = (val) => {
         setInputVal(val.target.value);
     }
@@ -23,13 +33,12 @@ const WeatherComponent = () => {
         async function getCoordinat() {
             const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`);
             const data = await response.json();
+            // cityName değeri geocoding Api'da bir şeyle eşleşmezse hata mesajı ver.
             if(!data[0]){
                 alert(`${cityName} not found!`);
                 return;
             }
-            console.log(data[0]);
             setCityItem(data[0]);
-            
         }
         getCoordinat();
     }, [cityName]);
@@ -42,43 +51,75 @@ const WeatherComponent = () => {
             return;
         }
         async function getCity() {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${cityItem?.lat}&lon=${cityItem?.lon}&appid=${apiKey}`);
-            const data = await response.json();
-            setWeatherItem(data);
-            console.log(data); 
+            try{
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${cityItem?.lat}&lon=${cityItem?.lon}&appid=${apiKey}`);
+                const data = await response.json();
+                setWeatherItem(data);
+            } catch{
+                alert("Error!");
+                return;
+            }
         }
         getCity();
-        
     }, [cityItem]);
 
 
-
+    // Input'tan gelen değer boş değil ise cityName'e ata
     const handleClick = () => {
         if(inputVal.trim() === ""){
             return;
         }
         setCityName(inputVal);
         setInputVal("");
-        
     }
+
+     // Checkbox ile tema durumunu güncelle
+    const handleChange = () => {
+        if(!checked){
+          setChecked(true);
+    
+          // CSS değişkenleri temanın durumuna göre güncellemek için obje döndür
+          for(const [key, value] of Object.entries(cssThemeObject)){
+            document.documentElement.style.setProperty(key, value.dark);
+          }
+    
+        } else{
+          setChecked(false);
+    
+          // CSS değişkenleri temanın durumuna göre güncellemek için obje döndür
+          for(const [key, value] of Object.entries(cssThemeObject)){
+            document.documentElement.style.setProperty(key, value.light);
+          }
+    
+        } 
+      }
 
     return(
         <div className="container">
-            <div className="box">
-                {/* Input + Submit Button */}
-                <div className="searchBox">
-                    <input type="text" value={inputVal} placeholder="Enter City Name" className="inputClass" onChange={getValue} 
-                        onKeyPress={(ev) => {
-                          if (ev.key === "Enter") { handleClick(); } }}
-                        >
-                    </input>
-                    <div className="searchBtn" onClick={() => handleClick()}>
-                        <img src={process.env.PUBLIC_URL + '/images/search.svg'} className="searchImg" />
-                    </div>
-                    
-                </div>
 
-                {/* Şehir Yazdır */}
+            {/* Input + Submit Button + Toggle*/}
+            <div className="box">
+                <div className="headBox">
+                    <div className="searchBox">
+                        <input type="text" value={inputVal} placeholder="Enter City Name" className="inputClass" onChange={getValue} 
+                            onKeyPress={(ev) => {
+                              if (ev.key === "Enter") { handleClick(); } }}
+                            >
+                        </input>
+                        <div className="searchBtn" onClick={() => handleClick()}>
+                            <img src={process.env.PUBLIC_URL + '/images/search.svg'} className="searchImg" />
+                        </div>
+                    </div>
+                    <div className="checkBox">
+                        <label className="switch">
+                          <input type="checkbox" name="themeSelector" onChange={() => handleChange()}/>
+                          <span className="slider round"></span>
+                        </label>
+                    </div>
+                </div>
+                
+
+                {/* Şehrin Hava Durumu Bilgilerini Yazdır */}
                 <div className="weatherInfo">
                 {
                     weatherItem && (
@@ -96,14 +137,8 @@ const WeatherComponent = () => {
                 } 
                 </div>
             </div>
-            
-
-            
         </div>
-        
-        
     )
-
 }
 
 export default WeatherComponent;
---
+++ src/cssThemeObject.js
@@ -0,0 +1,45 @@
+const cssThemeObject = {
+
+    '--bodyBackgroundColor': {
+        light: '#84e0fe',
+        dark: '#1a202e'
+      }
+     ,
+
+     '--boxBackgroundColor': {
+        light: '#bfefff',
+        dark: '#1c2230'
+      }
+     ,
+
+     '--inputBackgroundColor': {
+        light: '#2666CF',
+        dark: '#292847'
+      }
+     ,
+
+     '--borderColor': {
+        light: '#4682b4',
+        dark: '#27303f'
+      }
+     ,
+
+     '--inputTextColor': {
+        light: '#cae1ff',
+        dark: '#97a6ba'
+      }
+     ,
+
+     '--textColor': {
+        light: '#4876ff',
+        dark: '#e2e8f0'
+      }
+      ,
+
+      '--svgColor': {
+         light: 'invert(80%) sepia(25%) saturate(293%) hue-rotate(180deg) brightness(103%) contrast(102%)',
+         dark: 'invert(73%) sepia(6%) saturate(828%) hue-rotate(175deg) brightness(88%) contrast(94%)'
+       }
+};
+
+export default cssThemeObject;
